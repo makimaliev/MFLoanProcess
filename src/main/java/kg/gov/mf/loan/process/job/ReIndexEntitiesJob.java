6e37d72bd8a216fc1e7f7dab140206f8e5691362
Marlen Akimaliev <makimaliev@gmail.com>
2018-05-07 15:32:34 +0600

    ReIndex Entities job

---
+++ src/main/java/kg/gov/mf/loan/process/job/ReIndexEntitiesJob.java
@@ -0,0 +1,51 @@
+package kg.gov.mf.loan.process.job;
+
+import kg.gov.mf.loan.manage.service.debtor.DebtorService;
+import kg.gov.mf.loan.manage.util.DateUtils;
+import kg.gov.mf.loan.process.model.JobItem;
+import org.quartz.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Transactional
+@Component
+public class ReIndexEntitiesJob implements Job {
+
+    @Autowired
+    DebtorService debtorService;
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        JobItem job = getJobItemFromJobCaller(context);
+        if(job == null) throw new NullPointerException(); //Write Custom Exception
+        indexEntities();
+    }
+
+    private void indexEntities()
+    {
+        debtorService.indexEntity();
+    }
+
+    private JobItem getJobItemFromJobCaller(JobExecutionContext context)
+    {
+        JobItem job = null;
+
+        try
+        {
+            SchedulerContext schedulerContext = context.getScheduler().getContext();
+            job = (JobItem) schedulerContext.get("jobItem");
+
+        }
+        catch(SchedulerException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return job;
+    }
+
+}
